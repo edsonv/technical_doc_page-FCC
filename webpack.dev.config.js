2cccd8a381bdc60d26da91bb96ff869e4f674dbc
Edson J Vargas <edson.jhair@yahoo.com>
2018-08-27 15:21:57 -0400

    Se corrigió error en la disposición del código

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,4 +1,5 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin') const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 const WriteFilePlugin = require('write-file-webpack-plugin')
 
