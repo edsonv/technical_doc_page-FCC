f9ac10b6acf9ffb5394ebe0cc14e6f194f581794
Edson J Vargas <edson.jhair@yahoo.com>
2018-08-27 10:22:25 -0400

    Commit inicial

---
+++ postcss.config.js
@@ -0,0 +1,26 @@
+
+module.exports= {
+  plugins: [
+  require('postcss-import')( {
+    plugins: [ require('stylelint')]
+  }),
+  require('postcss-font-magician')( {
+    variants: {
+      'Lato': {
+        '300': [],
+        '400': [],
+      }
+    }
+  }),
+  require('postcss-preset-env')( {
+    stage: 2,
+    features: {
+      customProperties: false,
+      calc: false
+    },
+    browsers: 'last 2 versions'
+  }),
+  require('css-mqpacker'),
+  require('cssnano'),
+  ]
+}
